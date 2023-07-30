f5d93811965939a7d7e1f6c0292dfdc0c1885333
atre1 <atre1tech@gmail.com>
2023-07-30 17:19:41 +0300

    firstTry

---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV;
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        open: true,
+        hot: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    ],
+    module: {
+        rules: [
+            //babel
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            //html
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            //css
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader"
+                ],
+            },
+            //fonts
+            {
+                test: /\.woff2?$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[ext]'
+                }
+            },
+            //images compress
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
+            }
+        ],
+    },
+}
